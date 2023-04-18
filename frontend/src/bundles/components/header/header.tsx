0bd9c2ed89df4395da68a949cc42097a27c38d1d
Andrew <gandrejko2002@gmail.com>
2023-04-18 17:44:10 +0300

    add header

---
+++ frontend/src/bundles/components/header/header.tsx
@@ -0,0 +1,29 @@
+import styles from './header.module.scss';
+
+export const Header: React.FC = () => {
+  return (
+    <div className={styles.header}>
+      <div className={`${styles.headerContainer} container`}>
+        <div className={styles.logo}>logo</div>
+        <ul className={styles.navigation}>
+          <li className={styles.navigationItem}>
+            <a className={styles.navigationLink} href="#">Опитування</a>
+          </li>
+          <li className={styles.navigationItem}>
+            <a className={styles.navigationLink} href="#">Функціонал</a>
+          </li>
+          <li className={styles.navigationItem}>
+            <a className={styles.navigationLink} href="#">Передзамовлення</a>
+          </li>
+          <li className={styles.navigationItem}>
+            <a className={styles.navigationLink} href="#">Про нас</a>
+          </li>
+          <li className={styles.navigationItem}>
+            <a className={styles.navigationLink} href="#">Контакти</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+    ;
+}
